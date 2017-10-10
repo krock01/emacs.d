ccf3574b8ea2293489d8e2a9e1f8818e0351aa0b
krock01 <jack2569@163.com>
2017-10-10 17:09:12 +0800

    init set package manager in init.el

---
+++ init.el
@@ -0,0 +1,39 @@
+;; 去掉启动界面的信息
+(setq inhibit-startup-message t)
+
+
+(require 'package)
+;; 关闭原包的启动
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "http://elpa.emacs-china.org/melpa/"))
+
+(package-initialize)
+
+;; 启动'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package which-key
+	     :ensure t
+	     :config
+	     (which-key-mode))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
