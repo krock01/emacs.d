cc433ebec921e6b28f851a10b9b222a7101a6a17
krock01 <jack2569@163.com>
2017-10-10 20:38:39 +0800

    add windows operation

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key use-package))))
+ '(package-selected-packages (quote (ace-window which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,13 +29,32 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; key tip
 (use-package which-key
 	     :ensure t
 	     :config
 	     (which-key-mode))
 
+
+;; org-mode extension
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+
+;; window
+;;C-c left or right toggle other windows
+(winner-mode 1)
+;; shift-left down right up goto the window
+;;(windmove-default-keybindings)
+;; ace-window C-x o number
+(use-package ace-window
+  :ensure t
+  :init
+  (progn
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+    ))
