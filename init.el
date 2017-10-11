d85ee826698ecf7645dfe4df30ac1a7e4f0bee1a
krock01 <jack2569@163.com>
2017-10-11 08:00:05 +0800

    add theme

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (auto-complete try counsel ace-window which-key use-package))))
+    (monokai-theme auto-complete try counsel ace-window which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -103,3 +103,7 @@
     (ac-config-default)
     (global-auto-complete-mode t)
     ))
+;; theme
+(use-package monokai-theme
+  :ensure t
+  :config (load-theme 'monokai t))
