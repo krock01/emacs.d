d0a57cc9c688826cb20548d9fb8ed8031debdb1b
krock01 <jack2569@163.com>
2017-10-13 21:18:59 +0800

    hide scroll and max window

--- krock_init.el
+++ krock_init.el
@@ -1,5 +1,11 @@
 (setq inhibit-startup-message t)
+(set-scroll-bar-mode nil)
 (tool-bar-mode -1)
+(setq make-backup-files nil)
+(use-package maxframe
+:ensure t
+:config
+(add-hook 'window-setup-hook 'maximize-frame t))
 (fset 'yes-or-no-p 'y-or-n-p)
 (global-set-key (kbd "<f5>") 'revert-buffer)
 
