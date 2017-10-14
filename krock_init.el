73aef9c6da38accbd3bea43cfd272855120834b8
krock01 <jack2569@163.com>
2017-10-14 18:21:04 +0800

    add emmet

--- krock_init.el
+++ krock_init.el
@@ -270,6 +270,14 @@ narrowed."
             (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
             (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
 
+(use-package emmet-mode
+:ensure t
+:config
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'web-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+)
+
 (use-package projectile
   :ensure t
   :config
