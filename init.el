c21c916721f81a719f5a7245c6e38c427c52a212
krock <jack2569@163.com>
2017-10-21 21:35:18 +0800

    add packgae company and move require packages to init.el file

--- init.el
+++ init.el
@@ -3,9 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (moe-dark)))
  '(package-selected-packages
    (quote
-    (tide typescript-mode emmet-mode better-shell powerline dumb-jump counsel-projectile projectile treemacs-projectile treemacs neotree git-timemachine git-gutter magit ggtags dired-quick-sort dired+ nodejs-repl tern-auto-complete tern js2-refactor ac-js2 js2-mode web-mode iedit expand-region hungry-delete beacon undo-tree elpy jedi htmlize monokai-theme auto-complete try counsel ace-window which-key use-package)))
+    (js-doc yasnippet exec-path-from-shell tide typescript-mode emmet-mode better-shell powerline dumb-jump counsel-projectile projectile treemacs-projectile treemacs neotree git-timemachine git-gutter magit ggtags dired-quick-sort dired+ nodejs-repl tern-auto-complete tern js2-refactor ac-js2 js2-mode web-mode iedit expand-region hungry-delete beacon undo-tree elpy jedi htmlize monokai-theme auto-complete try counsel ace-window which-key use-package)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -27,6 +28,98 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;;(org-babel-load-file (expand-file-name "~/.emacs.d/krock_init.org"))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/krock_init.org"))
-(put 'upcase-region 'disabled nil)
+(require 'init-tweaks)
+
+(require 'init-try)
+
+(require 'init-which-key)
+
+(require 'init-ace)
+
+(require 'init-ivy)
+
+(require 'init-avy)
+
+;(require 'init-ac)
+(require 'init-cp)
+
+(require 'init-theme)
+
+(require 'init-reveal)
+
+;(setq python-shell-completion-native-enable nil)
+
+(require 'init-path-shell)
+
+;(use-package jedi
+;  :ensure t
+;  :init
+;  (add-hook 'python-mode-hook 'jedi:setup)
+;  (add-hook 'python-mode-hook 'jedi:ac-setup))
+
+;(use-package elpy
+;:ensure t
+;:config 
+;(elpy-enable))
+
+(require 'init-yasnippet)
+
+(require 'init-undo)
+
+(require 'init-misc)
+
+; mark and edit all copies of the marked region simultaniously. kbd C-;
+(require 'init-iedit)
+
+(require 'init-web)
+
+(require 'init-js)
+
+(require 'init-emmet)
+
+(require 'init-projectile)
+
+(require 'init-jump)
+
+;(use-package dired+
+;  :ensure t
+;  :config (require 'dired+)
+;  )
+
+
+;(use-package dired-quick-sort
+;  :ensure t
+;  :config
+;  (dired-quick-sort-setup))
+
+(require 'init-ibuffer)
+
+(require 'init-gtag)
+
+(require 'init-git)
+
+;; babel stuff
+;(org-babel-do-load-languages
+; 'org-babel-load-languages
+; '((python . t)
+;   (emacs-lisp . t)
+;   (C . t)
+;   (js . t)
+;   (dot . t)
+;   (org . t)
+;   ))
+
+(require 'init-tree)
+
+(require 'init-shell)
+
+(require 'init-typescript)
+
+(require 'init-js-doc)
+
+(require 'init-check)
+
+(provide 'init)
---
+++ lisp/init-cp.el
@@ -0,0 +1,7 @@
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  )
+
+(provide 'init-cp)
