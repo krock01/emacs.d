39dd228d90021090ae907099ed9269053e77a7a2
王栋 <wd@dorado.cc>
2017-11-02 10:07:12 +0800

    modify company-mode kbd "company-select-next" and "company-select-previous"

--- lisp/init-cp.el
+++ lisp/init-cp.el
@@ -1,5 +1,10 @@
 (use-package company
   :ensure t
+	  
+  :config (progn
+  	  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  	  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  	  )
 )
 
 (provide 'init-cp)
