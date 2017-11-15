f75da0317cc5fea41f727d6e3d1a6d6737705540
krock <jack2569@163.com>
2017-11-15 21:27:55 +0800

    modify company-mode minimum-prefix-length 1

--- lisp/init-cp.el
+++ lisp/init-cp.el
@@ -1,9 +1,11 @@
 (use-package company
   :ensure t
-	  
+  :ensure company-tern
   :config (progn
-  	  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  	  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+	    (add-to-list 'company-backends 'company-tern)
+	    (setq company-minimum-prefix-length 1)
+	    (define-key company-active-map (kbd "C-n") 'company-select-next)
+	    (define-key company-active-map (kbd "C-p") 'company-select-previous)
   	  )
 )
 
